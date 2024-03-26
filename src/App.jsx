b99be25f9cf3082a2e51fe0f7314e0655f69b9c6
LauraMariana27 <documentoslams@gmail.com>
2024-03-26 09:42:11 -0300

    Aula26.3

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,31 @@
-
 import './App.css'
+import Card from './components/Card'
+import img01 from "./assets/Imgs/img01.jpg"
+import img02 from "./assets/Imgs/img02.jpg"
+import img03 from "./assets/Imgs/img03.jpg"
+
+
 
 function App() {
 
   return (
     <>
-     <h1>Olá, mundo. Eu sou a Vite.</h1>
+      <Card title="Paisagens"
+      image={img01}
+      text1="Paisagem Escura e Misteriosa"
+      text2="Curitiba / PR"/>
+
+      <Card title="Paisagens"
+      image={img02}
+      text1="Paisagem Clara e Convidativa"
+      text2="Curitiba / PR"/>   
+
+      <Card title="Paisagens"
+      image={img03}
+      text1="Paisagem Clara e Gelada"
+      text2="Curitiba / PR"/>   
     </>
+    
   )
 }
 
